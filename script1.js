cd0e280d60f9e229020e90956c2863d79f87a24c
MariaLuiza1904 <0001202915@senaimgaluno.com.br>
2026-06-10 08:33:23 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+function boasVindas() {
+    alert("Bem-vindo ao programa!");
+}
+
+// chamando a função
+boasVindas();
---
+++ script2.js
@@ -0,0 +1,7 @@
+function soma(a, b) {
+    return a + b;
+}
+
+// exemplo de uso
+let resultado = soma(5, 3);
+alert(resultado);
